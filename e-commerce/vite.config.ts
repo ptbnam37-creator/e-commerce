19aecd709e1e566b153a9d0863a87faa1b210200
ptbnam37-creator <ptbnam37@gmail.com>
2026-06-15 14:46:00 +0700

    feat: make PocketBase URL configurable via environment variable VITE_POCKETBASE_URL

--- e-commerce/vite.config.ts
+++ e-commerce/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['.ngrok-free.dev', '.ngrok.io'],
+    allowedHosts: true,
   },
 })
 
