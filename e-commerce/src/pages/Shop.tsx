6b23d2caa2f283caa8b27fcc2dd8ddf1caa71bf2
ptbnam37-creator <ptbnam37@gmail.com>
2026-06-17 10:02:00 +0700

    style: change unfilled star color from grey to white

--- e-commerce/src/pages/Shop.tsx
+++ e-commerce/src/pages/Shop.tsx
@@ -26,7 +26,7 @@ const StarIcon = ({ fillPercent, size = '28px' }: StarIconProps) => {
       <defs>
         <linearGradient id={gradientId}>
           <stop offset={`${fillPercent * 100}%`} stopColor="#ffd214" />
-          <stop offset={`${fillPercent * 100}%`} stopColor="#e0e0e0" />
+          <stop offset={`${fillPercent * 100}%`} stopColor="#ffffff" />
         </linearGradient>
       </defs>
       <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" fill={`url(#${gradientId})`} />
