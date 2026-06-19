b07405acddc403093fc8891453dc68a6179aafdc
ptbnam37-creator <ptbnam37@gmail.com>
2026-06-19 11:21:02 +0700

    fix: update pocketbase api url to new railway deployment

--- e-commerce/src/services/pocketbase.ts
+++ e-commerce/src/services/pocketbase.ts
@@ -1,3 +1,3 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('https://e-commerce-backend-production-6151.up.railway.app');
+export const pb = new PocketBase('https://e-commerce-backend-production-afe0.up.railway.app');
