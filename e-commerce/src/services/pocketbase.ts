f2ddeb3c162b8e8c7d405e17d0c6b9e5df3ab890
ptbnam37-creator <ptbnam37@gmail.com>
2026-06-19 13:58:51 +0700

    fix: add ngrok-skip-browser-warning header to bypass interstitial page

--- e-commerce/src/services/pocketbase.ts
+++ e-commerce/src/services/pocketbase.ts
@@ -1,3 +1,11 @@
 import PocketBase from 'pocketbase';
 
 export const pb = new PocketBase('https://anymore-ship-onlooker.ngrok-free.dev');
+
+// Bypass ngrok browser warning interstitial page
+pb.beforeSend = function (url, options) {
+  options.headers = Object.assign({}, options.headers, {
+    'ngrok-skip-browser-warning': 'true',
+  });
+  return { url, options };
+};
